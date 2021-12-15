41a433ad297cc3fa3575e189155c540a730fe520
Emmanuelle Laguna <emanuelle.laguna@televisatim.com>
2021-12-15 09:18:29 -0600

    ADD: Bulk

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import * as XLSX from "xlsx";
 
-function App() {
+const readExcel = (event) => {
+  const { target: { files = [] } = {} } = event;
+  console.log("files", files);
+  const file = files[0];
+  console.log("file", file);
+  const promise = new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsArrayBuffer(file);
+    fileReader.onload = (e) => {
+      const bufferArray = e.target.result;
+      const workBook = XLSX.read(bufferArray, { type: "buffer" });
+      const sheetName = workBook.SheetNames[1];
+      const workSheet = workBook.Sheets[sheetName];
+      const data = XLSX.utils.sheet_to_json(workSheet);
+      resolve(data); 
+    };
+    fileReader.onerror = (error) => {
+      reject(error);
+    }
+  });
+  promise.then(d => console.log("data", d));
+}
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" onChange={readExcel} />
     </div>
-  );
+  )
 }
 
 export default App;
